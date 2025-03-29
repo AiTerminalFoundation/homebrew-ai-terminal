efa92263552c67c0a43b07e3128875e1953e79d2
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-03-29 01:55:05 +0100

    Update ai-terminal formula to version 0.2.0

--- Casks/ai-terminal.rb
+++ Casks/ai-terminal.rb
@@ -1,6 +1,6 @@
 cask "ai-terminal" do
   version "0.2.0"
-  sha256 "3404a8d96499764195ba3d3cc411824fa3732b7ea0aaf4b3329e7c45e6e7a4f8"
+  sha256 "bd98651acc9cd028f4cc6bc6f28f3696fdcb8708659af31477737a02f41c227a"
 
   url "https://github.com/AiTerminalFoundation/ai-terminal/releases/download/v#{version}/ai-terminal-#{version}.dmg"
   name "AI Terminal"
